928ba22340db81c1686020d0408f0f2e7887f341
A9-dev <henrypearson422@gmail.com>
2020-03-25 03:59:27 +0000

    Added text

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ function httpGet(theUrl) {
 function buttonClicked() {
     var data = JSON.parse(httpGet("https://api.nasa.gov/planetary/apod?api_key=TKvp8w4LLObGl3wInVKmIzNGTlCNoOKgIjagqL24"))
     console.log(data);
-    var htmlStr = `<img src="${data.hdurl}" alt="${data.title}" class="img-fluid" id="spaceImage">`
-    document.getElementById("mainID").innerHTML = htmlStr
+    var imageStr = `<img src="${data.hdurl}" alt="${data.title}" class="img-fluid" id="spaceImage">`
+    document.getElementById("mainID").innerHTML = imageStr
+    var infoStr = `<p class="display-4">${data.title}</p><blockquote class="blockquote"><p class="mb-0">${data.explanation}</p><footer class="blockquote-footer">${data.date}</cite></footer></blockquote>`
+    document.getElementById("infoArea").innerHTML = infoStr;
+    document.getElementById("buttonContainer").innerHTML = "";
+
 }
