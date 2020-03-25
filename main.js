8bc86bcbd583baa8a46afb88baa8fcf7fa02fb88
A9-dev <henrypearson422@gmail.com>
2020-03-25 05:13:50 +0000

    Added copyright and removed console log of http response

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ function httpGet(theUrl) {
 
 function buttonClicked() {
     var data = JSON.parse(httpGet("https://api.nasa.gov/planetary/apod?api_key=TKvp8w4LLObGl3wInVKmIzNGTlCNoOKgIjagqL24"))
-    console.log(data);
     var imageStr = `<img src="${data.url}" alt="${data.title}" class="img-fluid" id="spaceImage">`
     document.getElementById("mainID").innerHTML = imageStr
-    var infoStr = `<p class="display-4">${data.title}</p><blockquote class="blockquote"><p class="mb-0">${data.explanation}</p><footer class="blockquote-footer">${data.date}</cite></footer></blockquote>`
+    var infoStr = `<p class="display-4">${data.title}</p><blockquote class="blockquote"><p class="mb-0">${data.explanation}</p><footer class="blockquote-footer">${data.date} ${data.copyright}</cite></footer></blockquote>`
     document.getElementById("infoArea").innerHTML = infoStr;
     document.getElementById("buttonContainer").innerHTML = "";
 
